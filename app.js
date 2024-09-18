f77f38b98486b6a3062d6c7c8c934b60ab189e5c
SyedTousif <stousif1020@gmail.com>
2024-09-18 12:47:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Docker Container on Azure! This is a demo setup created from GitHub Actions where the image is pushed to Azure Container Registry and deployed to Azure App Service.');
+    res.send('Hello from Docker Container on Azure! This is a demo setup created from GitHub Actions where the image is pushed to Azure Container Registry and deployed to Azure App Service. Thank you');
 });
 
 app.listen(port, () => {
